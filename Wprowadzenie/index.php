6ad74d124641a935a10d4b483d9f94aaa53fdeb1
Alekwit <alekwit@gmail.com>
2020-11-19 13:34:32 +0100

    Wprowadzenie

---
+++ Wprowadzenie/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>
+        Zad 1 PHP
+    </title>
+</head>
+    <body>
+        <?php 
+            include 'pierwszy.php';
+            echo "</br>";
+            include 'drugi.php';
+        ?>
+    </body>
+</html>
