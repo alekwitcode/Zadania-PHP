ad190a78772d3b816961f65cdcf29e86b7636146
alekwitcode <52920009+alekwitcode@users.noreply.github.com>
2020-11-21 00:14:29 +0100

    Update ciasteczkoPOST.php
    Posted on phone, not sure if it fixes anything

--- Witryna/src/ciasteczkoPOST.php
+++ Witryna/src/ciasteczkoPOST.php
@@ -1,9 +1,7 @@
-
 <?php
-    $domain = ($_SERVER['HTTP_HOST'] != 'localhost') ? $_SERVER['HTTP_HOST'] : false;
-    if($_POST['send-imie']) {
+    if(isset($_POST['send-imie'])) {
         $cookie_value = $_POST['imie'];
-        setcookie('imie', $cookie_value, time()+60*60*24*30, '/', $domain, false);
+        setcookie('imie', $cookie_value, time()+60*60*24*30, '/', 'localhost');
     }
-    header("Location: http://localhost:8089/");
+    header("Location: ./index.php");
 ?>
